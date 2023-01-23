1aa61bdd61c2d4386b4ad9e78b4d5a672db93664
Evans-mutuku <evansnyamai98@gmail.com>
2023-01-23 19:50:45 +0300

    done with navigation bar

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger")
+const navLinks = document.querySelector(".nav__link")
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle("hide")
+})
